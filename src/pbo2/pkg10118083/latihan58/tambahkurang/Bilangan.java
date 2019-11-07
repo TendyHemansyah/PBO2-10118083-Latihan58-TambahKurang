303f82b4c91a555505bdf18b6cd12608f763f2a0
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-11-07 21:40:15 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118083/latihan58/tambahkurang/Bilangan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118083.latihan58.tambahkurang;
+
+/**
+ *
+ * @author         
+ * Nama     : Tendy Hermansyah
+ * Kelas    : IF-2
+ * Nim      : 10118083
+ * Deskripsi Tugas : Tambah Kurang
+ */
+public class Bilangan {
+    private int x, y;
+    
+    public Bilangan() {
+        x = 3;
+        y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/pbo2/pkg10118083/latihan58/tambahkurang/PBO2_10118083_Latihan58_TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118083.latihan58.tambahkurang;
+
+/**
+ *
+ * @author         
+ * Nama     : Tendy Hermansyah
+ * Kelas    : IF-2
+ * Nim      : 10118083
+ * Deskripsi Tugas : Tambah Kurang
+ */
+public class PBO2_10118083_Latihan58_TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumlahBilangan = new JumlahBilangan();
+        jumlahBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan selisihBilangan = new SelisihBilangan();
+        selisihBilangan.tampilSelisih();
+    }
+    
+}
